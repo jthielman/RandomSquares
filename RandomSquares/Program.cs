7cb6c2774ad6285578f99a4e86ee55023f190424
feven90 <f.mulugeta90@gmail.com>
2020-03-30 20:46:25 -0500

    Add project files.

---
+++ RandomSquares/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random numbers n squarin em n stuff");
+
+            var random = new Random();
+            var randomNumbers = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var currentNumber = random.Next(1, 51);
+                randomNumbers.Add(currentNumber);
+            }
+
+            var squaredRandos = new List<int>();
+
+            foreach (int rando in randomNumbers)
+            {
+                squaredRandos.Add(rando * rando);
+            }
+
+            foreach (int number in randomNumbers)
+            {
+                Console.Write(number.ToString() + "  ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int number in squaredRandos)
+            {
+                Console.Write(number.ToString() + "  ");
+            }
+
+            Console.WriteLine();
+
+            var onlyEvens = squaredRandos.Where(rando => rando % 2 == 0);
+
+            foreach (int number in onlyEvens)
+            {
+                Console.Write(number.ToString() + "  ");
+            }
+        }
+    }
+}
